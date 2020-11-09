b31c4d74e99d396ec81dceb6df272e91e30ce87e
lukaneto <luizcsoaresneto@gmail.com>
2020-11-09 08:26:58 -0300

    primeiro commit

--- target/classes/static/sigma/listaNotificacao.js
+++ target/classes/static/sigma/listaNotificacao.js
@@ -56,22 +56,7 @@ function notificacoesLista(page){
 		elems = pagina['notificacao'];	
 		adicionaNotificacao(elems,'notification-item-visualizado',page+1,maxPagina);
 		
-	}
-
-	if(page!==''&& page !==undefined){
-	
-//	if(page==maxPagina){
-//				var botaoCarregarMais = "<span class='' >Sem mais notificacoes</span>"; 
-//				$('.notifications-wrapper').append(botaoCarregarMais);	
-//			}else{
-//				var botaoCarregarMais = "<a type='button' class='view-more-button' onclick='carregaProximaPagina(this,"+page+1+")'>Ver mais</a>"; 
-//				$('.notifications-wrapper').append(botaoCarregarMais);
-//			}
-//		
-//	}
-		
-	
-	
+	}	
 }	
 
 	
@@ -82,8 +67,7 @@ function carregaProximaPagina(objet, page){
 		
 function adicionaNotificacao(elems,config,proximaPagina,maxPagina){
 	 $.each(elems, function(item,el) {
-	 	
-		var notificacao = "<a class='content abrirModal'  data-dados='"+ JSON.stringify(el)+"'"
+	 	var notificacao = "<a class='content abrirModal'  data-dados='"+ JSON.stringify(el)+"'"
 			  +" onclick='abrirModal($(this))' >"
 		      + "<div class="+ config+">"
 			  +" <h4 class='item-title'>"+ el.titulo+"</h4>"
@@ -93,102 +77,75 @@ function adicionaNotificacao(elems,config,proximaPagina,maxPagina){
 			  + "</a>";
 			 
 		 	$('.notifications-wrapper').append(notificacao);
-	});
-
-	
+	});	
 }
 
 
 
-function abrirModal(div)
-{
+function abrirModal(div){
+
 	$('#modal_body').find("div").remove(); 
-
 	var dados = div.data('dados');
-
 	console.log(dados.id);
-	
 	requisicaoAjax('/notificacao/visualizar/'+dados.id,"POST");
-	
-	
 	var el = dados;
 	console.log(dados);
 	$('#modal').modal('show');
 	$('#modal_title').text(dados.titulo);
-	
 	var body = "<div class='row'> <div class='col-sm-12'>  <p>"+dados.descricao+" </p> </div></div>";
-	
 	$('#modal_body').append(body);
-	
 	var imagemElement="";
-	
-	imagemElement = "<div class='row'><div class='col-sm-12'>"+carregaImagens(dados['listaImagem'])+"</div></div>";
-	
+	imagemElement = "<div class='row'><div class='col-sm-12'>"+carregaImagens(dados['listaImagem'])+"</div></div>";	
 	$('#modal_body').append(imagemElement);
-	
-	
 	var linkElemento ="";
 	$.each(dados['listaLink'], function(item,el) {
 		linkElemento =  "<div class='row'><div class='col-sm-12'> <a href='"+el.link+"' target='_blank'> "+el.titulo+"</a> </div> </div>";
 		$('#modal_body').append(linkElemento);
 	});
 	
-	console.log(linkElemento);
-	
-	
 }
 
 
 function carregaImagens(listaImagens){
-
-if(listaImagens!= undefined && listaImagens!=null && listaImagens.length>0){
-	
-	var carrossel = "<div id='myCarousel' class='carousel slide' data-ride='carousel'>"
-	+"<ol class='carousel-indicators'>";
-	
-	$.each(listaImagens, function(item,el) {
-		var li ="";
-		 if(item ==0){
-	 		li = "<li data-target='#myCarousel' data-slide-to='"+item+"' class='active'></li>";
-		}else{
-			li = "<li data-target='#myCarousel' data-slide-to='"+item+"'></li>";
-		}
-		carrossel = carrossel + li;
-	});
-	
-	carrossel= carrossel +"</ol>";
-	 carrossel = carrossel + "<div class='carousel-inner'>";
-	 
-	 
-	 $.each(listaImagens, function(item,el) {
-		var li ="";
-		var imagemExibir = 'http://127.0.0.1:8887/'+el.imagem.replace("C:","").replace(/[\\"]/g,"/");
-		if(item ==0){
-	 		li = "<div class='item active'> <img src='"+ imagemExibir +"'  alt='"+el.descricao+"'> </div>";
-		}else{
-			li = "<div class='item'> <img src='"+ imagemExibir +"'  alt='"+el.descricao+"'> </div>";
-		}
-		carrossel = carrossel + li;
-	});
-	
-	carrossel = carrossel + "</div>";
-	 
-	  
-	carrossel = carrossel +"<a class='left carousel-control' href='#myCarousel' data-slide='prev'>"
-	   + "<span class='glyphicon glyphicon-chevron-left'>  </span>"
-	   + "<span class='sr-only'>Previous</span>"
-	  +"</a>"
-	  +"<a class='right carousel-control' href='#myCarousel' data-slide='next'>"
-	  +" <span class='glyphicon glyphicon-chevron-right'></span>"
-	   +"<span class='sr-only'>Next</span>"
-	  +"</a>"
-	+"</div>";
-	
-	
-	
-	return carrossel;
-	
-	}
-return '';
-}
+	if(listaImagens!= undefined && listaImagens!=null && listaImagens.length>0){
+		var carrossel = "<div id='myCarousel' class='carousel slide' data-ride='carousel'><ol class='carousel-indicators'>";
+		$.each(listaImagens, function(item,el) {
+			var li ="";
+			 if(item ==0){
+		 		li = "<li data-target='#myCarousel' data-slide-to='"+item+"' class='active'></li>";
+			}else{
+				li = "<li data-target='#myCarousel' data-slide-to='"+item+"'></li>";
+			}
+			carrossel = carrossel + li;
+		});
 		
+		carrossel= carrossel +"</ol>";
+		 carrossel = carrossel + "<div class='carousel-inner'>";
+		 
+		 
+		 $.each(listaImagens, function(item,el) {
+			var li ="";
+			var imagemExibir = 'http://127.0.0.1:8887/'+el.imagem.replace("C:","").replace(/[\\"]/g,"/");
+			if(item ==0){
+		 		li = "<div class='item active'> <img src='"+ imagemExibir +"'  alt='"+el.descricao+"'> </div>";
+			}else{
+				li = "<div class='item'> <img src='"+ imagemExibir +"'  alt='"+el.descricao+"'> </div>";
+			}
+			carrossel = carrossel + li;
+		});
+		
+		carrossel = carrossel + "</div>";
+		carrossel = carrossel +"<a class='left carousel-control' href='#myCarousel' data-slide='prev'>"
+		   + "<span class='glyphicon glyphicon-chevron-left'>  </span>"
+		   + "<span class='sr-only'>Previous</span>"
+		  +"</a>"
+		  +"<a class='right carousel-control' href='#myCarousel' data-slide='next'>"
+		  +" <span class='glyphicon glyphicon-chevron-right'></span>"
+		   +"<span class='sr-only'>Next</span>"
+		  +"</a>"
+		+"</div>";
+			return carrossel;
+	}
+	return '';
+}
+	
