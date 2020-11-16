ddcd1bb4df3527d5d3f1933ad4c6b9c3fe90f42d
maboo94 <mabootecnologia@gmail.com>
2020-11-16 16:57:31 -0300

    imagem aparecendo assincrono

---
+++ src/main/resources/static/sigma/script.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+        $(input).next()
+        .attr('src', e.target.result)
+    };
+    reader.readAsDataURL(input.files[0]);
+    }
+    else {
+        var img = input.value;
+        $(input).next().attr('src',img);
+    }
+} 
+
+function verificaMostraBotao(){
+    $('input[type=file]').each(function(index){
+        if ($('input[type=file]').eq(index).val() != ""){
+            readURL(this);
+            $('.hide').show();
+        }
+    });
+}
+
+$('input[type=file]').on("change", function(){
+  verificaMostraBotao();
+});
+
+$('.hide').on("click", function(){
+    $(document.body).append($('<input />', {type: "file" }).change(verificaMostraBotao));
+    $(document.body).append($('<img />'));
+    $('.hide').hide();
+});
